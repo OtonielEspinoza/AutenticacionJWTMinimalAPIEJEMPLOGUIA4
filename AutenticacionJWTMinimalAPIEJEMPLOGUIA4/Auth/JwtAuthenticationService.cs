6645527821aa4ca3a8b89782883580f80ffd0821
Otoniel Esteban Espinoza Contreras <esteban15espinosa@outlook.com>
2023-09-12 22:25:53 -0600

    finalizar miercoles

--- AutenticacionJWTMinimalAPIEJEMPLOGUIA4/Auth/JwtAuthenticationService.cs
+++ AutenticacionJWTMinimalAPIEJEMPLOGUIA4/Auth/JwtAuthenticationService.cs
@@ -11,8 +11,28 @@ namespace AutenticacionJWTMinimalAPIEJEMPLOGUIA4.Auth
 {
     public class JwtAuthenticationService : IJwtAuthenticationService
     {
+        private readonly string _key;
+
+        public JwtAuthenticationService(string key)
+        {
+            _key = key;
+        }
+
+        public string Authenticate(string userName)
+        {
 
 
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var tokenKey = Encoding.ASCII.GetBytes(_key);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim )
+
+            };
+            throw new NotImplementedException();
+        }
     }
    
 }
